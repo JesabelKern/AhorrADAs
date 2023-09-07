0d7ee43f39402e1c0ea9b18b1b84bfc794205941
Jesabel Kern <jesabelkern@gmail.com>
2023-09-07 17:00:10 -0300

    function button cancel

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const listaCategorias = (category) => {
         <li class="tag has-text-weight-semibold is-flex is-justify-content-space-between">
                 <p>${nombre}</p>
                 <div class="column is-narrow has-text">
-                    <a href="#" class="mr-4 is-size-7 edit-link">Editar</a>
+                    <a href="#" class="mr-4 is-size-7 edit-link editor-button">Editar</a>
                     <a href="#" class="is-size-7 delete-link">Eliminar</a>
                 </div>
         </li>`
@@ -49,3 +49,24 @@ const listaCategorias = (category) => {
 }
 
 listaCategorias(categories)
+
+
+
+
+
+// AGREGAR CATEGORÍA
+
+
+
+//EDITAR CATEGORIA
+
+all('.editor-button').forEach( (btn) => {
+    btn.addEventListener('click', () => 
+    just('#edit-categories').classList.remove("is-hidden"));
+    
+
+});
+
+all('#cancel-edit-category-btn').forEach( (btn) => {
+    btn.addEventListener('click', () => just('#edit-categories').classList.add ("is-hidden"))
+});
